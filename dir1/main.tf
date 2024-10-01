518e44ff8c42ad5b4724b57e90b4e6ad581ee65c
Samrat Priyadarshi <sampriyadarshi@google.com>
2024-10-01 14:31:34 +0530

    add provider

--- dir1/main.tf
+++ dir1/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0" # Or the latest version
+    }
+  }
+}
+
 resource "random_id" "test" {
   byte_length = 8
 }
